4f988c620f2081484a6516e06e9518e095e98ed2
salas <salas@salas.local>
2025-05-12 10:48:05 -0500

    Codigo

---
+++ src/servicios/ServicioDocumento.java
@@ -0,0 +1,117 @@
+package servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import entidades.Documento;
+
+public class ServicioDocumento {
+
+    private static List<Documento> documentos = new ArrayList<>();
+
+    public static void cargar(String nombreArchivo) {
+        var br = Archivo.abrirArchivo(nombreArchivo);
+        if (br != null) {
+            try {
+                var linea = br.readLine();
+                linea = br.readLine();
+                while (linea != null) {
+                    var textos = linea.split(";");
+                    var documento = new Documento(textos[0], textos[1], textos[2], textos[3]);
+                    documentos.add(documento);
+                    linea = br.readLine();
+                }
+            } catch (Exception ex) {
+
+            }
+        }
+    }
+
+    public static void mostrar(JTable tbl) {
+        String[] encabezados = new String[] { "#", "Primer Apellido", "Segundo Apellido", "Nombres", "Documento" };
+        String[][] datos = new String[documentos.size()][encabezados.length];
+
+        int fila = 0;
+        for (var documento : documentos) {
+            datos[fila][0] = String.valueOf(fila + 1);
+            datos[fila][1] = documento.getApellido1();
+            datos[fila][2] = documento.getApellido2();
+            datos[fila][3] = documento.getNombre();
+            datos[fila][4] = documento.getDocumento();
+            fila++;
+        }
+
+        var dtm = new DefaultTableModel(datos, encabezados);
+        tbl.setModel(dtm);
+    }
+
+    private static boolean esMayor(Documento d1, Documento d2, int criterio) {
+        if (criterio == 0) {
+            return d1.getNombreCompleto().compareTo(d2.getNombreCompleto()) > 0 ||
+                    (d1.getNombreCompleto().equals(d2.getNombreCompleto()) &&
+                            d1.getDocumento().compareTo(d2.getDocumento()) > 0);
+        } else {
+            return d1.getDocumento().compareTo(d2.getDocumento()) > 0 ||
+                    (d1.getDocumento().equals(d2.getDocumento()) &&
+                            d1.getNombreCompleto().compareTo(d2.getNombreCompleto()) > 0);
+        }
+    }
+
+    private static void intercambiar(int origen, int destino) {
+        if (0 <= origen && origen < documentos.size() &&
+                0 <= destino && destino < documentos.size()) {
+            var temporal = documentos.get(origen);
+            documentos.set(origen, documentos.get(destino));
+            documentos.set(destino, temporal);
+        }
+    }
+
+    public static void ordenarBurbuja(int criterio) {
+        for (int i = 0; i < documentos.size() - 1; i++) {
+            for (int j = i + 1; j < documentos.size(); j++) {
+                System.out.println(
+                        "d[i]=" + documentos.get(i).getNombreCompleto() + " " + documentos.get(i).getDocumento());
+                System.out.println(
+                        "d[j]=" + documentos.get(j).getNombreCompleto() + " " + documentos.get(j).getDocumento());
+
+                if (esMayor(documentos.get(i), documentos.get(j), criterio)) {
+                    intercambiar(i, j);
+                }
+            }
+        }
+    }
+
+    private static int getPivote(int inicio, int fin, int criterio) {
+        int pivote = inicio;
+        var documentoPivote = documentos.get(pivote);
+
+        for (int i = inicio + 1; i <= fin; i++) {
+            if (esMayor(documentoPivote, documentos.get(i), criterio)) {
+                pivote++;
+                if (i != pivote) {
+                    intercambiar(i, pivote);
+                }
+            }
+        }
+        if (inicio != pivote) {
+            intercambiar(inicio, pivote);
+        }
+
+        return pivote;
+    }
+
+    private static void ordenarRapido(int inicio, int fin, int criterio) {
+        if (fin > inicio) {
+            var pivote = getPivote(inicio, fin, criterio);
+            ordenarRapido(inicio, pivote - 1, criterio);
+            ordenarRapido(pivote + 1, fin, criterio);
+        }
+    }
+
+    public static void ordenarRapido(int criterio) {
+        ordenarRapido(0, documentos.size() - 1, criterio);
+    }
+}
